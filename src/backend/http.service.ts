7e0b626273f78b7ee689fadae76f3d0352e3cc59
Iha2 <iheatudeveloper@gmail.com>
2018-09-23 09:30:10 -0400

    filtering

--- src/backend/http.service.ts
+++ src/backend/http.service.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { from } from 'rxjs';
 
-import { Customer } from './http.types';
+import { CustomerResponse, TransactionResponse } from './http.types';
 import { TDHeaders, customersUrl } from '../config';
 
 export const getAllTransactions = (customer: string) =>
   from(
-    axios.get<Customer>(`${customersUrl}/${customer}/transactions`, TDHeaders)
+    axios.get<TransactionResponse>(`${customersUrl}/${customer}/transactions`, TDHeaders)
   );
 
 export const getCustomer = (customer: string) =>
-  from(axios.get<Customer>(`${customersUrl}/${customer}`, TDHeaders));
+  from(axios.get<CustomerResponse>(`${customersUrl}/${customer}`, TDHeaders));
